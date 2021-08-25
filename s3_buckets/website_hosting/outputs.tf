6bf3dc9cf96c98fd856cdbc94bf900c1c2d56631
Zach Cyrus <zscyrus31@gmail.com>
2021-08-25 15:58:39 -0400

    Edited outputs file

--- s3_buckets/website_hosting/outputs.tf
+++ s3_buckets/website_hosting/outputs.tf
@@ -1,11 +1,11 @@
 output "website_bucket_name" {
   description = "Name of the bucket"
-  value       = aws_s3_bucket.site.id
+  value       = aws_s3_bucket.static-website.id
 }
 
 output "bucket_endpoint" {
   description = "Bucket endpoint"
-  value       = aws_s3_bucket.site.website_endpoint
+  value       = aws_s3_bucket.static-website.website_endpoint
 }
 
 output "domain_name" {
