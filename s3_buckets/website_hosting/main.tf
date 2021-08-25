80f611e3782e7dc42a3cf094998705b9df81ee14
Zach Cyrus <zscyrus31@gmail.com>
2021-08-25 18:13:42 -0400

    Updated lifecycle

--- s3_buckets/website_hosting/main.tf
+++ s3_buckets/website_hosting/main.tf
@@ -18,6 +18,10 @@ resource "aws_s3_bucket" "static-website" {
   bucket = var.bucket_name
   acl    = "public-read"
   policy = data.aws_iam_policy_document.bucket_policy.json
+  force_destroy = true
+  lifecycle {
+    prevent_destroy = false
+  }
 
   website {
     index_document = "index.html"
