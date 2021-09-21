7cd039b86f7eb2ed702f46355b196c2124dac894
Zach Cyrus <zscyrus31@gmail.com>
2021-09-21 14:48:37 -0400

    Updated s3 module to contain a bucket policy

--- modules/s3-static-website/main.tf
+++ modules/s3-static-website/main.tf
@@ -13,3 +13,22 @@ resource "aws_s3_bucket" "static-website" {
   }
 
 }
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.static-website.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource = [
+          aws_s3_bucket.static-website.arn,
+          "${aws_s3_bucket.static-website.arn}/*",
+        ]
+      },
+    ]
+  })
+}
